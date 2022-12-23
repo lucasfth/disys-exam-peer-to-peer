833ae6ed9b9cf9057c5047c02e89397194c0a6d1
Lucasfth <luha@itu.dk>
2022-12-23 11:17:38 +0100

    make go mod tidy

---
+++ peer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	request "Lucasfth/disys-exam-peer-to-peer/grpc/grpc"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+
+
